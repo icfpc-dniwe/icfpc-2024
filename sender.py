08031b5964c413ce4b8a7967cf8c06db545d50a6
Andrei Shadrikov <notvuvko@gmail.com>
2024-06-28 18:25:28 +0500

    sender

---
+++ sender.py
@@ -0,0 +1,36 @@
+import os
+from dotenv import load_dotenv
+import requests
+from interpreter import ICFPInterpreter
+from encoder import encode_message, encode_string
+
+# Load environment variables from .env file
+load_dotenv()
+
+
+def send_icfp_request(icfp_program, authorization_token):
+    url = 'https://boundvariable.space/communicate'
+    encoded_message = "S'%4}).$%8" #encode_message(icfp_program)
+    print(f'Encoded message: `{encoded_message}`')
+    headers = {
+        'Authorization': f'Bearer {authorization_token}',
+        'Content-Type': 'application/json'
+    }
+    payload = {
+        'message': encode_message(icfp_program)
+    }
+    
+    response = requests.post(url, headers=headers, json=payload)
+    if response.status_code != 200:
+        print('Response error!', response.status_code, '\n', response.text)
+        return None
+    else:
+        return ICFPInterpreter(response.text.strip()).evaluate()
+
+
+if __name__ == '__main__':
+    icfp_program = [
+        encode_string('get index')
+    ]
+    print('Returned', send_icfp_request(icfp_program, os.getenv('ACCESS_TOKEN')))
+    
