47f625ec434914a93b5fb32872756974bfb12b4e
Andrei Shadrikov <notvuvko@gmail.com>
2024-06-29 15:43:15 +0500

    more spaceship (but sending is not working correctly)

--- solve_spaceship.sh
+++ solve_spaceship.sh
@@ -1,5 +1,6 @@
-prefix="solve spaceship1 "
-solution=$(echo spaceship/1.txt | python spaceship.py)
+number=$1
+prefix="solve spaceship$number "
+solution=$(echo spaceship/$number.txt | python spaceship.py)
 message=$(echo $prefix$solution | python encoder.py)
 echo $message
 echo $(echo $message | python interpreter.py)
